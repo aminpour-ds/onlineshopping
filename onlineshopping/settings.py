a02877cd9c09c9b1e0bd1ddf7d27e66ac06edf95
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-13 21:44:01 +0330

    add template and static files

--- onlineshopping/settings.py
+++ onlineshopping/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'store/static',
+]
+
+STATIC_ROOT = BASE_DIR / 'assets'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
