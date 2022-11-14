72399081317affb57df5a0512557be64bd49739b
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-14 21:36:49 +0330

    registering models of store app in admin.py file

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    list_display = ['description', 'discount', 'start_date', 'end_date']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price', 'collection', 'inventory']
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['phone', 'membership']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer']
+
+
+@admin.register(models.OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['quantity', 'unit_price', 'product']
