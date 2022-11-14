139be82e794b65fe7aeff23d393cad7d92d2709a
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-14 23:52:16 +0330

    addin list_editable and list_per_page for pages in admin

--- store/admin.py
+++ store/admin.py
@@ -8,6 +8,8 @@ from . import models
 @admin.register(models.Promotion)
 class PromotionAdmin(admin.ModelAdmin):
     list_display = ['description', 'discount', 'start_date', 'end_date']
+    list_editable = ['discount', 'end_date']
+    list_per_page = 10
 
 
 @admin.register(models.Collection)
@@ -18,18 +20,24 @@ class CollectionAdmin(admin.ModelAdmin):
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'price', 'collection', 'inventory']
+    list_editable = ['price', 'inventory']
+    list_per_page = 10
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['phone', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
 
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['placed_at', 'payment_status', 'customer']
+    list_per_page = 10
 
 
 @admin.register(models.OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ['quantity', 'unit_price', 'product']
+    list_per_page = 10
