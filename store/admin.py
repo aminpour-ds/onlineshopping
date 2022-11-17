859c52cbcb8c52cb339fbbfef6c143989546a6f4
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-17 23:23:12 +0330

    addig orderitem in order form

--- store/admin.py
+++ store/admin.py
@@ -37,6 +37,7 @@ class ProductAdmin(admin.ModelAdmin):
                     'collection', 'inventory', 'inventory_status']
     list_editable = ['price', 'inventory']
     list_filter = ['collection', 'last_update']
+    search_fields = ['title']
     list_per_page = 10
 
     @admin.display(ordering='inventory')
@@ -74,9 +75,17 @@ class CustomerAdmin(admin.ModelAdmin):
         return super().get_queryset(request).annotate(orders_count=Count('orders'))
 
 
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    autocomplete_fields = ['product']
+    min_num = 1
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['placed_at', 'payment_status', 'customer']
     list_per_page = 10
 
