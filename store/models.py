148da72d3f91a1bd19562cf99ffd57bdf1fda2a3
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-20 15:52:34 +0330

    Create model Review

--- store/models.py
+++ store/models.py
@@ -134,3 +134,10 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.order.id)
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
