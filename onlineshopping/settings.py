89336043a40a7e967339cd4fd3ec1068dfb43be7
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-22 01:14:11 +0330

    installing Djoser and SIMPLE_JWT for authentication json-web-token

--- onlineshopping/settings.py
+++ onlineshopping/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'debug_toolbar',
     'store',
     'tags',
@@ -151,4 +152,9 @@ AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication', ),
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
