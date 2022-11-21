aa3815de2329b6a805b42be8bd7041df9cdea5b9
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-21 15:58:08 +0330

    setting REST_FRAMEWORK config for not changeing DECIMAL_TO_STRING

--- onlineshopping/settings.py
+++ onlineshopping/settings.py
@@ -147,3 +147,8 @@ STATIC_ROOT = BASE_DIR / 'assets'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+}
