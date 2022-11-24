ccdb128cb9af9eb9f49db7d98c17fd6db0d82681
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-24 13:03:48 +0330

    Getting the Current User

--- onlineshopping/settings.py
+++ onlineshopping/settings.py
@@ -163,5 +163,6 @@ SIMPLE_JWT = {
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer'
     }
 }
