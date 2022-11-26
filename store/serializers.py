6d15fb6264e3471dc49ef2b99c649a2640973864
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-26 13:02:49 +0330

    change the create method to save

--- store/serializers.py
+++ store/serializers.py
@@ -58,10 +58,10 @@ class AddCartItemSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('there is no product with this id')
         return value
     
-    def create(self, validated_data):
+    def save(self, **kwargs):
         cart_id = self.context['cart_id']
-        product_id = validated_data['product_id']
-        quantity = validated_data['quantity']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
         try:
             cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
             cart_item.quantity += quantity
