9b164b5bcc562d04442976dc0cf637c14dfb4a5b
Mina Aminpour <mina.aminpur@gmail.com>
2022-11-26 20:56:09 +0330

    change the minimum value of inventory of product to zero

--- store/models.py
+++ store/models.py
@@ -34,7 +34,7 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=6, decimal_places=2, validators=[MinValueValidator(1)])
     inventory = models.IntegerField(
-        validators=[MinValueValidator(1)])  # موجودی
+        validators=[MinValueValidator(0)])  
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
         Collection, on_delete=models.PROTECT, related_name='products')
